0379f56409117c037acd0a1ab810ae55d70609b4
HyoYoung0829 <gydud7300@naver.com>
2025-09-23 11:38:16 +0900

    chore : 파일 디렉토리 구성 밎 템플릿 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/next';
 import localFont from 'next/font/local';
-import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
