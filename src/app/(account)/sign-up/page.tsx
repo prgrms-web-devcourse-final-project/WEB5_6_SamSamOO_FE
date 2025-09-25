94f27d34c54f911c266c4eeb2fa046ebb51579dd
HyoYoung0829 <gydud7300@naver.com>
2025-09-25 15:46:48 +0900

    style : 계정찾기, 비밀번호 재설정 페이지 제작

--- src/app/(account)/sign-up/page.tsx
+++ src/app/(account)/sign-up/page.tsx
@@ -10,7 +10,9 @@ function Page() {
     <div className="center-col">
       <div className="center-col">
         <h2 className="mb-2 text-4xl font-bold">👋 환영합니다!</h2>
-        <h3 className="mb-9 text-2xl">지금부터 바로가 든든한 동반자가 되어드릴게요!</h3>
+        <h3 className="mb-9 text-2xl break-keep text-center">
+          지금부터 바로가 든든한 동반자가 되어드릴게요!
+        </h3>
       </div>
 
       <form className="center-col mb-10 w-[420px] gap-7">
