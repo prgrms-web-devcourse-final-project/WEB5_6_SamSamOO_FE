31d3c69f59bff55bd1fe224a6107bb584683c68a
YooJeong01 <ujjh77@naver.com>
2025-09-25 20:36:47 +0900

    chore : 검색 페이지 폴더 구조 구성

---
+++ src/app/search/(search-layout)/total/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '바로 | 통합 검색',
+  description: '바로 BaLaw 법령 판례 검색 페이지입니다',
+};
+
+function Page() {
+  return <div>통합검색 결과</div>;
+}
+export default Page;
