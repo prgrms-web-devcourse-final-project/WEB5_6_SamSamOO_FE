c4e6d43bd9e7c30c6defcb95cc0a657854c4ea5b
HyoYoung0829 <gydud7300@naver.com>
2025-09-25 14:59:28 +0900

    style : 다크모드 캐럿 색상 수정

--- src/components/ui/account/AccountInput.tsx
+++ src/components/ui/account/AccountInput.tsx
@@ -9,6 +9,8 @@ interface Props {
 export const STYLE = tw(
   'h-13 w-full rounded-full px-6 outline-none',
   'bg-[#F4F4F4] placeholder-primary-black shadow-[inset_0_4px_10px_0_rgba(0,0,0,0.3)]',
+
+  'dark:caret-primary-black',
 );
 
 function AccountInput({ placeholder, type, onChange }: Props) {
