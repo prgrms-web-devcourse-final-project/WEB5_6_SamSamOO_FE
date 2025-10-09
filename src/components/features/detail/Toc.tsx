b4cc515690dc625cfb6959134a3a7f5641ab8b54
YooJeong Hwang <ujjh77@gmail.com>
2025-10-10 01:49:57 +0900

    style : toc active 스타일 추가

--- src/components/features/detail/Toc.tsx
+++ src/components/features/detail/Toc.tsx
@@ -1,7 +1,10 @@
 import { TableOfContent } from '@/types/detail';
+import tw from '@/utils/tw';
+import { useState } from 'react';
 
 function Toc({ toc }: { toc: TableOfContent }) {
-  console.log(toc);
+  const [target, setTarget] = useState<string>('');
+  // console.log(toc);
   return (
     <nav>
       <ul className="min-w-[290px] sm:w-[366px] flex flex-col gap-y-4 px-8">
@@ -11,11 +14,20 @@ function Toc({ toc }: { toc: TableOfContent }) {
               <details key={`${id}${text}`}>
                 <summary className="font-medium text-xl pb-4">{text}</summary>
                 <p className="flex flex-col gap-2">
-                  <a href={id} className="pl-6 text-[18px]">
+                  <a
+                    href={id}
+                    className={tw('pl-6 text-[18px] ', id === target && 'text-brand-accent')}
+                    onClick={() => setTarget(id)}
+                  >
                     {text}
                   </a>
                   {children?.map(({ id, text }) => (
-                    <a href={id} key={`${id}${text}`} className="pl-6 text-[18px]">
+                    <a
+                      href={id}
+                      key={`${id}${text}`}
+                      className={tw('pl-6 text-[18px]', id === target && 'text-brand-accent')}
+                      onClick={() => setTarget(id)}
+                    >
                       {text}
                     </a>
                   ))}
@@ -24,7 +36,12 @@ function Toc({ toc }: { toc: TableOfContent }) {
             );
           } else
             return (
-              <a href={id} key={`${id}${text}`} className="font-medium text-xl pb-4">
+              <a
+                href={id}
+                key={`${id}${text}`}
+                className={tw('font-medium text-xl pb-4', id === target && 'text-brand-accent')}
+                onClick={() => setTarget(id)}
+              >
                 {text}
               </a>
             );
