ec8a358a86ae97a258e32761bc80a14c7e1af687
YooJeong Hwang <ujjh77@gmail.com>
2025-10-09 22:01:04 +0900

    style : 필터 모달 반응형

--- src/components/features/search/LawSearchFilterModal.tsx
+++ src/components/features/search/LawSearchFilterModal.tsx
@@ -44,144 +44,159 @@ function LawSearchFilterModal({ isOpen = false, onClose = () => {}, setLawSearch
     isOpen: !!openCalendar,
     onClose: () => setOpenCalendar(null),
     ignoreSelectors: ['[data-radix-popper-content-wrapper]'],
+    hiddenOverflow: false,
   });
 
   return (
     <div
-      className="absolute inset-0 z-10 bg-black/40 backdrop-blur-sm w-screen h-screen flex items-center justify-center"
+      className="fixed flex inset-0 z-10 bg-black/40 backdrop-blur-sm w-screen h-screen items-center justify-center"
       onPointerDown={(e) => {
         if (e.target === e.currentTarget) onClose();
       }}
     >
-      <section className="p-10 bg-white rounded-modal flex flex-col gap-6 text-lg justify-center dark:bg-background-black3 dark:text-primary-white dark:shadow-modal-dark">
-        <h1 className="text-3xl font-bold text-brand-primary dark:text-primary-white">
-          법령 상세 검색
-        </h1>
-        <div className="flex gap-7 items-center">
-          <label className="min-w-fit" htmlFor="lawField">
-            법령분야
-          </label>
-          <SelectMenu
-            field="lawField"
-            value={lawField}
-            itemList={lawFieldData}
-            triggerStyle="w-[165px]"
-            onValueChange={(value) => {
-              if (value !== '전체') setLawField(value);
-            }}
-            onOpenChange={(open) => {
-              if (open) setOpenCalendar(null);
-            }}
-          />
-        </div>
-        <div className="flex gap-7 items-center">
-          <label className="min-w-fit" htmlFor="authority">
-            소관부처
-          </label>
-          <SelectMenu
-            field="authority"
-            value={authority}
-            itemList={authorityData}
-            triggerStyle="w-[165px]"
-            onValueChange={(value) => {
-              setAuthority(value);
-            }}
-            onOpenChange={(open) => {
-              if (open) setOpenCalendar(null);
-            }}
-          />
-          <SelectMenu
-            field="ministry"
-            key={authority}
-            value={ministry}
-            itemList={ministryList}
-            triggerStyle="w-[230px]"
-            onValueChange={(value) => {
-              setMinistry(value);
-            }}
-            onOpenChange={(open) => {
-              if (open) setOpenCalendar(null);
-            }}
-            disabled={!authority || authority === '전체'}
-            placeholder={
-              authority === '전체' ? '전체' : authority ? `${authority} 선택` : '먼저 소관기관 선택'
-            }
-          />
-        </div>
-
-        <div ref={promulgationRef} className="relative flex gap-7 items-center">
-          <label htmlFor="promulgation" className="min-w-fit">
-            공포일자
-          </label>
-          <button
-            id="promulgation"
-            type="button"
-            onClick={() => {
-              setOpenCalendar(openCalendar === 'promulgation' ? null : 'promulgation');
-            }}
-            className="w-full py-2 px-6 flex gap-3 items-center justify-between rounded-full bg-background-white  outline-none border border-filter-outline1 text-primary-gray2 focus:shadow-filter-light-active dark:bg-primary-gray3 dark:text-primary-white dark:border-filter-outline2 data-[state=open]:shadow-filter-light-active dark:data-[state=open]:shadow-filter-dark-active'"
-          >
-            {promulgationRange.start === null || promulgationRange.end === null
-              ? '기간을 선택해주세요'
-              : `${promulgationRange.start} ~ ${promulgationRange.end}`}
-            <FilterDetailDown className="dark:text-[#6E6E6E]" />
-          </button>
-          {openCalendar === 'promulgation' && (
-            <div className="absolute top-13 right-0 z-10">
-              <CalendarWithDateInputRange onChangeValue={setPromulgationRange} />
+      <section className="w-full h-screen overflow-y-scroll sm:overflow-y-visible sm:w-fit sm:h-fit px-10 pt-30 pb-10 sm:px-10 sm:py-10 bg-white sm:rounded-modal sm:mb-5  dark:bg-background-black3 dark:text-primary-white dark:shadow-modal-dark">
+        <div className="flex flex-col gap-6 text-lg justify-center ">
+          <h1 className="text-3xl font-bold text-brand-primary dark:text-primary-white">
+            법령 상세 검색
+          </h1>
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-7 sm:items-center">
+            <label className="min-w-fit" htmlFor="lawField">
+              법령분야
+            </label>
+            <SelectMenu
+              field="lawField"
+              value={lawField}
+              itemList={lawFieldData}
+              triggerStyle="w-[165px]"
+              onValueChange={(value) => {
+                setLawField(value);
+              }}
+              onOpenChange={(open) => {
+                if (open) setOpenCalendar(null);
+              }}
+            />
+          </div>
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-7 sm:items-center">
+            <label className="min-w-fit" htmlFor="authority">
+              소관부처
+            </label>
+            <div className="flex flex-col gap-4 sm:flex-row sm:gap-7">
+              <SelectMenu
+                field="authority"
+                value={authority}
+                itemList={authorityData}
+                triggerStyle="w-[165px]"
+                onValueChange={(value) => {
+                  setAuthority(value);
+                }}
+                onOpenChange={(open) => {
+                  if (open) setOpenCalendar(null);
+                }}
+              />
+              <SelectMenu
+                field="ministry"
+                key={authority}
+                value={ministry}
+                itemList={ministryList}
+                triggerStyle="w-[230px]"
+                onValueChange={(value) => {
+                  setMinistry(value);
+                }}
+                onOpenChange={(open) => {
+                  if (open) setOpenCalendar(null);
+                }}
+                disabled={!authority || authority === '전체'}
+                placeholder={
+                  authority === '전체'
+                    ? '전체'
+                    : authority
+                      ? `${authority} 선택`
+                      : '먼저 소관기관 선택'
+                }
+              />
             </div>
-          )}
-        </div>
+          </div>
 
-        <div ref={enforcementRef} className="relative flex gap-7 items-center">
-          <label htmlFor="enforcement" className="min-w-fit">
-            시행일자
-          </label>
-          <button
-            id="enforcement"
-            type="button"
-            onClick={() => {
-              setOpenCalendar(openCalendar === 'enforcement' ? null : 'enforcement');
-            }}
-            className="w-full py-2 px-6 flex gap-3 items-center justify-between rounded-full bg-background-white  outline-none border border-filter-outline1 text-primary-gray2 focus:shadow-filter-light-active dark:bg-primary-gray3 dark:text-primary-white dark:border-filter-outline2 data-[state=open]:shadow-filter-light-active dark:data-[state=open]:shadow-filter-dark-active'"
+          <div
+            ref={promulgationRef}
+            className="relative flex flex-col sm:flex-row gap-2 sm:gap-7 sm:items-center"
           >
-            {enforcementRange.start === null || enforcementRange.end === null
-              ? '기간을 선택해주세요'
-              : `${enforcementRange.start} ~ ${enforcementRange.end}`}
-            <FilterDetailDown className="dark:text-[#6E6E6E]" />
-          </button>
-          {openCalendar === 'enforcement' && (
-            <div className="absolute top-13 right-0 z-10">
-              <CalendarWithDateInputRange onChangeValue={setEnforcementRange} />
-            </div>
-          )}
-        </div>
+            <label htmlFor="promulgation" className="min-w-fit">
+              공포일자
+            </label>
+            <button
+              id="promulgation"
+              type="button"
+              onClick={() => {
+                setOpenCalendar(openCalendar === 'promulgation' ? null : 'promulgation');
+              }}
+              className="w-full py-2 px-6 text-sm sm:text-[16px] flex gap-3 items-center justify-between rounded-full bg-background-white  outline-none border border-filter-outline1 text-primary-gray2 focus:shadow-filter-light-active dark:bg-primary-gray3 dark:text-primary-white dark:border-filter-outline2 data-[state=open]:shadow-filter-light-active dark:data-[state=open]:shadow-filter-dark-active'"
+            >
+              {promulgationRange.start === null || promulgationRange.end === null
+                ? '기간을 선택해주세요'
+                : `${promulgationRange.start} ~ ${promulgationRange.end}`}
+              <FilterDetailDown className="dark:text-[#6E6E6E]" />
+            </button>
+            {openCalendar === 'promulgation' && (
+              <div className="absolute top-22 sm:top-13 sm:right-0 z-10 w-full sm:w-[82%]">
+                <CalendarWithDateInputRange onChangeValue={setPromulgationRange} />
+              </div>
+            )}
+          </div>
 
-        <div className="flex gap-4 items-center justify-center">
-          <button
-            type="button"
-            className="px-11 py-3 bg-brand-primary text-primary-white font-bold rounded-full dark:bg-primary-white dark:text-primary-black"
-            onClick={() => {
-              setLawSearchFilter({
-                lawField,
-                ministry,
-                promulgationDateStart: promulgationRange.start ?? '',
-                promulgationDateEnd: promulgationRange.end ?? '',
-                enforcementDateStart: enforcementRange.start ?? '',
-                enforcementDateEnd: enforcementRange.end ?? '',
-              });
-              onClose();
-            }}
+          <div
+            ref={enforcementRef}
+            className="relative flex flex-col sm:flex-row gap-2 sm:gap-7 sm:items-center mb-5"
           >
-            검색
-          </button>
-          <button
-            type="button"
-            className="px-11 py-3 border border-brand-primary font-bold rounded-full dark:border-primary-white"
-            onClick={onClose}
-          >
-            닫기
-          </button>
+            <label htmlFor="enforcement" className="min-w-fit">
+              시행일자
+            </label>
+            <button
+              id="enforcement"
+              type="button"
+              onClick={() => {
+                setOpenCalendar(openCalendar === 'enforcement' ? null : 'enforcement');
+              }}
+              className="w-full py-2 px-6 text-sm sm:text-[16px] flex gap-3 items-center justify-between rounded-full bg-background-white  outline-none border border-filter-outline1 text-primary-gray2 focus:shadow-filter-light-active dark:bg-primary-gray3 dark:text-primary-white dark:border-filter-outline2 data-[state=open]:shadow-filter-light-active dark:data-[state=open]:shadow-filter-dark-active'"
+            >
+              {enforcementRange.start === null || enforcementRange.end === null
+                ? '기간을 선택해주세요'
+                : `${enforcementRange.start} ~ ${enforcementRange.end}`}
+              <FilterDetailDown className="dark:text-[#6E6E6E]" />
+            </button>
+            {openCalendar === 'enforcement' && (
+              <div className="absolute bottom-14 sm:top-13 sm:right-0 z-10 w-full sm:w-[82%]">
+                <CalendarWithDateInputRange onChangeValue={setEnforcementRange} />
+              </div>
+            )}
+          </div>
+
+          <div className="flex gap-4 items-center justify-center">
+            <button
+              type="button"
+              className="px-9 sm:px-11 py-3 bg-brand-primary text-primary-white font-bold rounded-full dark:bg-primary-white dark:text-primary-black"
+              onClick={() => {
+                setLawSearchFilter({
+                  lawField,
+                  ministry,
+                  promulgationDateStart: promulgationRange.start ?? '',
+                  promulgationDateEnd: promulgationRange.end ?? '',
+                  enforcementDateStart: enforcementRange.start ?? '',
+                  enforcementDateEnd: enforcementRange.end ?? '',
+                });
+                onClose();
+              }}
+            >
+              검색
+            </button>
+            <button
+              type="button"
+              className="px-9 sm:px-11 py-3 border border-brand-primary font-bold rounded-full dark:border-primary-white"
+              onClick={onClose}
+            >
+              닫기
+            </button>
+          </div>
         </div>
       </section>
     </div>
