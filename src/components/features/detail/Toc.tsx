43948ae66ba85afebf9d0028772cdefcaffbc60f
YooJeong Hwang <ujjh77@gmail.com>
2025-10-14 10:04:36 +0900

    fix : 상세 toc 스크롤 방식 수정
    - url 이동이 아니라 직접 이동으로

--- src/components/features/detail/Toc.tsx
+++ src/components/features/detail/Toc.tsx
@@ -5,6 +5,16 @@ import { useState } from 'react';
 function Toc({ toc }: { toc: TableOfContent }) {
   const [target, setTarget] = useState<string>('');
 
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement>, id: string) => {
+    e.preventDefault();
+    setTarget(id);
+    console.log(id);
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+
   return (
     <nav>
       <ul className="min-w-[290px] md:w-[366px] flex flex-col gap-y-4 md:pl-8 pr-12 text-primary-gray2 dark:text-primary-white">
@@ -17,7 +27,7 @@ function Toc({ toc }: { toc: TableOfContent }) {
                   <a
                     href={id}
                     className={tw('pl-6 text-[18px] ', id === target && 'text-brand-accent')}
-                    onClick={() => setTarget(id)}
+                    onClick={(e) => handleScroll(e, id)}
                   >
                     {text}
                   </a>
@@ -26,7 +36,7 @@ function Toc({ toc }: { toc: TableOfContent }) {
                       href={id}
                       key={`${id}${text}`}
                       className={tw('pl-6 text-[18px]', id === target && 'text-brand-accent')}
-                      onClick={() => setTarget(id)}
+                      onClick={(e) => handleScroll(e, id)}
                     >
                       {text}
                     </a>
@@ -40,7 +50,7 @@ function Toc({ toc }: { toc: TableOfContent }) {
                 href={id}
                 key={`${id}${text}`}
                 className={tw('font-medium text-xl pb-4', id === target && 'text-brand-accent')}
-                onClick={() => setTarget(id)}
+                onClick={(e) => handleScroll(e, id)}
               >
                 {text}
               </a>
